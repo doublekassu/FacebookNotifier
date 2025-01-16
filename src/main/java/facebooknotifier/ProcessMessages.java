2d589543cc5336509b9213ea9b49005a3530f6d9
doublekassu <kasimir.alamaki@gmail.com>
2025-01-16 17:52:24 +0200

    Posts containing keywords are opened automatically. A list keeps track of them as well

--- src/main/java/facebooknotifier/ProcessMessages.java
+++ src/main/java/facebooknotifier/ProcessMessages.java
@@ -1,13 +1,12 @@
 package facebooknotifier;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.awt.Desktop;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.ListMessagesResponse;
@@ -15,20 +14,19 @@ import com.google.api.services.gmail.model.Message;
 
 public class ProcessMessages {
     private ArrayList<String> openedPosts;
+    private ArrayList<String> triggeredPosts;
+
     public ProcessMessages() {
         openedPosts = new ArrayList<>();
+        triggeredPosts = new ArrayList<>();
     }
     
 
     public void processMessages(Gmail service) throws IOException {
         int counter = 0;
-        List<String> keyWordList = Arrays.asList("0.5", "0,5", "empire", "koli", "coin", "8", "90", "91", "92", "93", "94");
-        ArrayList<String> triggeredPosts = new ArrayList<>();
 
         ListMessagesResponse messagesResponse = service.users().messages().list("me")
-                .setLabelIds(Collections.singletonList("INBOX"))  // Oletustunniste, voit vaihtaa tarpeen mukaan
-                .setQ("category:personal")
-                .execute();
+                .setLabelIds(Collections.singletonList("Label_6175409980112628348")).execute();
 
         if (messagesResponse.getMessages() == null || messagesResponse.getMessages().isEmpty()) {
             System.out.println("Messages weren't found");
@@ -44,21 +42,15 @@ public class ProcessMessages {
                     String imgTxt = rawMessage.substring(rawMessage.lastIndexOf("Hei Kasimir") + 13, rawMessage.lastIndexOf("=3D=3D=3D") - 121);
                     String postId = rawMessage.substring(rawMessage.lastIndexOf("Message-ID: <") + 13, rawMessage.lastIndexOf("Message-ID: <") + 29);
                     System.out.println(imgTxt);
-                    /*for (int i=0; i<keyWordList.size(); i++) {
-                        if (imgTxt.contains(keyWordList.get(i))) {
-                            if (!triggeredPosts.contains(fullMessage.getId())) {
-                                triggeredPosts.add(fullMessage.getId() + " keyword: " + keyWordList.get(i));
-                                break;
-                            }
-                        }
-                    }*/
 
                     checkPostList(postId);
+                    checkKeywords(imgTxt, postId);
                     System.out.println("----------------------------------------------------------------------------------------");
                 }
                 counter++;
             }
             System.out.println("THESE POSTS HAVE BEEN ALREADY OPENED: " + openedPosts);
+            System.out.println("\nTRIGGERED POSTS: " + triggeredPosts);
         }
     }
 
@@ -73,11 +65,63 @@ public class ProcessMessages {
         }
     }
 
+    public void checkKeywords(String imgTxt, String postId) {
+        boolean containsKeyWord = false;
+
+        //Check if the triggered post has already been added to the list
+        for (String listElement : triggeredPosts) {
+            if (listElement.contains(postId)) {
+                return;
+            }
+        }
+        
+        //Check if imgtxt has Empire keywords 
+        List<String> empireKeyWords = Arrays.asList("0.5", "0,5", "empire", "koli", "coin");
+        for (int i=0; i<empireKeyWords.size(); i++) {
+            if (imgTxt.contains(empireKeyWords.get(i))) {
+                triggeredPosts.add(postId + ": EMPIRE");
+                containsKeyWord = true;
+                break;
+            }
+        }
+
+        //Check if has Buff keywords
+        if (checkStringForNumberBetween(imgTxt)) {
+            triggeredPosts.add(postId + ": BUFF");
+            containsKeyWord = true;
+        }
+
+        if (containsKeyWord) {
+            openFacebookPost(postId);
+        }
+    }
+
+    public boolean checkStringForNumberBetween(String imgTxt) {
+        int minNumber = 80;
+        int maxNumber = 94;
+
+        //Regex that finds all numbers from msg
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(imgTxt);
+
+        boolean checkNumbers = false;
+
+        //Integrate all numbers
+        while (matcher.find()) {
+            int number = Integer.parseInt(matcher.group());
+
+            if (number >= minNumber && number <= maxNumber) {
+                checkNumbers = true;
+                break;
+            }
+        }
+        return checkNumbers;
+    }
+
     //Checks opened posts list and adds if new
     public void checkPostList(String postId) {
         if (!openedPosts.contains(postId)) {
             openedPosts.add(postId);
-            openFacebookPost(postId);
         }
     }
 }
---
+++ src/main/java/facebooknotifier/TriggeredPostAlerter.java
@@ -0,0 +1,5 @@
+package facebooknotifier;
+
+public class TriggeredPostAlerter {
+    
+}
