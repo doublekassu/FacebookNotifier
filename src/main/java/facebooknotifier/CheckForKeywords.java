7f8de7cf4ecc63913e5e8b34bd594543442eaf8f
doublekassu <kasimir.alamaki@gmail.com>
2025-05-15 17:22:33 +0300

    Merge

--- src/main/java/facebooknotifier/CheckForKeywords.java
+++ src/main/java/facebooknotifier/CheckForKeywords.java
@@ -6,22 +6,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.File;
 
 public class CheckForKeywords {
-
-    //Lisää keywords.txt olevat stringit listaan ja checkKeywords funktiossa tarkastetaan listaa läpi
-
     private ArrayList<String> triggeredPosts;
     private TriggeredPostAlerter triggeredPostAlerter;
 
     private ArrayList<String> keyWordsTxtList;
+    private ArrayList<Float> keyNumbersList;
     private HashMap<String, List<String>> keyWordCategoryMap = new HashMap<>();
+    private HashMap<String, List<Float>> keyNumberCategoryMap = new HashMap<>();
 
     public CheckForKeywords()  {
         triggeredPosts = new ArrayList<>();
         keyWordsTxtList = new ArrayList<>();
+        keyNumbersList = new ArrayList<>();
         keyWordLineToList();
+        keyNumberLineTolist();
 
         try {
             triggeredPostAlerter = new TriggeredPostAlerter();
@@ -72,21 +75,97 @@ public class CheckForKeywords {
         }
     }
 
-    /*private void keyWordTxtToList()  {
-        Scanner keyWordScanner;
+    public void keyNumberLineTolist() {
+        String categoryKey = null;
+
+        Scanner fileScanner;
         try {
-            keyWordScanner = new Scanner(new File("./settings/keywords.txt"));
-            
-            //Skipping the first value of the line
-            keyWordScanner.next();
-            
-            while (keyWordScanner.hasNext()) {
-                keyWordsTxtList.add(keyWordScanner.next());
+            fileScanner = new Scanner(new File("./settings/keynumbers.txt"));
+            while (fileScanner.hasNextLine()) {
+                int lineIterator = 0;
+                String row = fileScanner.nextLine();
+                Scanner rowScanner = new Scanner(row);  
+                String value;
+    
+                while (rowScanner.hasNext()) {
+                    value = rowScanner.next();
+                    if (lineIterator == 0) {
+                        categoryKey = value;
+                    }
+                    else {
+                        keyNumbersList.add(Float.parseFloat(value));
+                    }
+                    lineIterator++;
+                }
+                //A copy is taken of the row's arraylist and put into a hashmap with the category name as the key
+                List<Float> keyNumberListCopy = new ArrayList<>(keyNumbersList);
+                keyNumberCategoryMap.put(categoryKey, keyNumberListCopy);
+                
+                //Original arraylist is cleared for the next row
+                keyNumbersList.clear();
+    
+                //RowsScanner can be opened and closed on every iteration. (System.in scanners can only be closed once during execution)
+                rowScanner.close();
+    
             }
-            keyWordScanner.close();
+    
+            System.out.println("Keynumbers: " + keyNumberCategoryMap);
         } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    //Check if the post's text includes numbers between the set keynumbers
+    /*public boolean checkStringForNumberBetween(String imgTxt) {
+        String categoryKey = null;
+
+        Scanner fileScanner = new Scanner(new File("./settings/keynumbers.txt"));
+
+        while (fileScanner.hasNextLine()) {
+            int lineIterator = 0;
+            String row = fileScanner.nextLine();
+            Scanner rowScanner = new Scanner(row);  
+            String value;
+
+            while (rowScanner.hasNext()) {
+                value = rowScanner.next();
+                if (lineIterator == 0) {
+                    categoryKey = value;
+                }
+                else {
+                    keyNumbersList.add(Float.parseFloat(value));
+                }
+                lineIterator++;
+            }
+            //A copy is taken of the row's arraylist and put into a hashmap with the category name as the key
+            List<Float> keyNumberListCopy = new ArrayList<>(keyNumbersList);
+            keyNumberCategoryMap.put(categoryKey, keyNumberListCopy);
+            
+            //Original arraylist is cleared for the next row
+            keyNumbersList.clear();
+
+            //RowsScanner can be opened and closed on every iteration. (System.in scanners can only be closed once during execution)
+            rowScanner.close();
+
+        }
+        
+        //Regex that finds all numbers from msg
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(imgTxt);
+
+        boolean checkNumbers = false;
+
+        //Integrate all numbers
+        while (matcher.find()) {
+            long number = Long.parseLong(matcher.group());
+
+            if (number >= minNumber && number <= maxNumber) {
+                checkNumbers = true;
+                break;
+            }
+        }
+        return checkNumbers;
     }*/
 
     public ArrayList<String> getTriggeredPosts() {
@@ -134,27 +213,4 @@ public class CheckForKeywords {
             triggeredPostAlerter.newPostAlertDiscord("1330963084965056616", imgTxt, postId, postLink, categoryName);
         }
     }
-
-    //Check if the post's text includes numbers between the set keynumbers
-    /*public boolean checkStringForNumberBetween(String imgTxt) {
-        long minNumber = 70;
-        long maxNumber = 94;
-
-        //Regex that finds all numbers from msg
-        Pattern pattern = Pattern.compile("\\d+");
-        Matcher matcher = pattern.matcher(imgTxt);
-
-        boolean checkNumbers = false;
-
-        //Integrate all numbers
-        while (matcher.find()) {
-            long number = Long.parseLong(matcher.group());
-
-            if (number >= minNumber && number <= maxNumber) {
-                checkNumbers = true;
-                break;
-            }
-        }
-        return checkNumbers;
-    }*/
 }
--- src/main/java/facebooknotifier/KeyWords.java
+++ src/main/java/facebooknotifier/KeyWords.java
@@ -10,6 +10,8 @@ import java.awt.Desktop;
 public class KeyWords {
 
     private File keyWordFile = new File("./settings/keywords.txt");
+    private File keyNumberFile = new File("./settings/keynumbers.txt");
+    private Desktop desktop = Desktop.getDesktop();
 
     public void runMenu() throws IOException {
         while (true) {
@@ -19,8 +21,11 @@ public class KeyWords {
             if (menuChoice.equals("1")) {
                 changeKeyWords();
             }
+            else if (menuChoice.equals("2")) {
+                setNumberBetweenTwoNumbers();
+            }
             else if (menuChoice.equals("3")) {
-                printKeyWords();
+                //printKeyWords();
             }
             else if (menuChoice.equals("0")) {
                 break;
@@ -31,19 +36,30 @@ public class KeyWords {
         }
     }
 
-    private void printKeyWords() throws IOException {
-        Scanner scanner = new Scanner(Paths.get("./keywords.txt"), StandardCharsets.UTF_8.name());
+    //Change to exclude the category name
+    /*private void printKeyWords() throws IOException {
+        Scanner scanner = new Scanner(Paths.get("./settings/keywords.txt"), StandardCharsets.UTF_8.name());
                 String content = scanner.useDelimiter("\\A").next();
                 scanner.close();
                 System.out.println("\nCurrent keywords set: " + content);
-    }
+    }*/
 
     private void changeKeyWords() {
         System.out.println("\nSet keywords that will send you a notification on Discord if they're included in a Facebook post! The keywords are categorized by lines. The first word of each line is the name of the category and it's not included in the scans! Please leave a whitespace between each keyword!\nExample of keyword category: category_name keyword1 keyword2");
-        Desktop desktop = Desktop.getDesktop();
         if (keyWordFile.exists()) {
             try {
                 desktop.open(keyWordFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setNumberBetweenTwoNumbers() {
+        System.out.println("\nSet two numbers in the file with a space in between them. The program will scan facebook posts with a number between the set numbers.");
+        if (keyNumberFile.exists()) {
+            try {
+                desktop.open(keyNumberFile);
             } catch (IOException e) {
                 e.printStackTrace();
             }
